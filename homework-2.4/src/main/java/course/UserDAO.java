846130ab67ed4c46fcee322ac9da90b94c6a6643
demi <idd90i@gmail.com>
2018-01-22 21:44:15 +0300

    hw 2.4 - implemented necessary methods for UserDao

--- homework-2.4/src/main/java/course/UserDAO.java
+++ homework-2.4/src/main/java/course/UserDAO.java
@@ -48,15 +48,19 @@ public class UserDAO {
         // create an object suitable for insertion into the user collection
         // be sure to add username and hashed password to the document. problem instructions
         // will tell you the schema that the documents must follow.
+		Document user = new Document("_id", username)
+			.append("password", passwordHash);
 
-        if (email != null && !email.equals("")) {
+		if (email != null && !email.equals("")) {
             // XXX WORK HERE
             // if there is an email address specified, add it to the document too.
+			user.append("email", email);
         }
 
         try {
             // XXX WORK HERE
             // insert the document into the user collection here
+			usersCollection.insertOne(user);
             return true;
         } catch (MongoWriteException e) {
             if (e.getError().getCategory().equals(ErrorCategory.DUPLICATE_KEY)) {
@@ -68,10 +72,9 @@ public class UserDAO {
     }
 
     public Document validateLogin(String username, String password) {
-        Document user = null;
-
         // XXX look in the user collection for a user that has this username
         // assign the result to the user variable.
+		Document user = usersCollection.find(eq("_id", username)).first();
 
         if (user == null) {
             System.out.println("User not in database");
