215a9a32286439f57f95e2a3812d03399d351d58
demi <idd90i@gmail.com>
2018-01-31 19:31:45 +0300

    implemented hw 3.3

--- homework-3.2/src/main/java/course/BlogPostDAO.java
+++ homework-3.2/src/main/java/course/BlogPostDAO.java
@@ -3,6 +3,8 @@ package course;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
 import com.mongodb.client.model.Sorts;
+import com.mongodb.client.model.Updates;
+import org.apache.commons.lang3.StringUtils;
 import org.bson.Document;
 
 import java.util.ArrayList;
@@ -93,5 +95,17 @@ public class BlogPostDAO {
         // - email is optional and may come in NULL. Check for that.
         // - best solution uses an update command to the database and a suitable
         //   operator to append the comment on to any existing list of comments
+		if (StringUtils.isEmpty(name)) {
+			throw new IllegalArgumentException("Name should be not empty.");
+		}
+		if (StringUtils.isEmpty(body)) {
+			throw new IllegalArgumentException("Body of comment should be not empty.");
+		}
+
+		Document comment = new Document("author", name).append("body", body);
+		if (StringUtils.isNotEmpty(email)) {
+			comment.append("email", email);
+		}
+		postsCollection.updateOne(eq("permalink", permalink), Updates.push("comments", comment));
     }
 }
